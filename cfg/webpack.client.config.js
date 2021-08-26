58c704c54a1c6d0ecf8a4fde421a597cd9caf11a
ShGulnaz <shakirova139@gmail.com>
2021-08-26 15:47:30 +0300

    add hmr

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { HotModuleReplacementPlugin} = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
@@ -12,15 +14,23 @@ function setupDevtool() {
 
 module.exports = {
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', 'tsx', '.json']
+        extensions: ['.js', '.jsx', '.ts', 'tsx', '.json'],
+        //вместо реакт дом, мы отдаем другие зависимости
+        alias: {
+            'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+        }
     },
     mode: NODE_ENV ? NODE_ENV : 'development',
     //откуда начинать
-    entry: path.resolve(__dirname, '../src/client/index.jsx'),
+    entry: [
+        path.resolve(__dirname, '../src/client/index.jsx'),
+        'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+    ],
     //куда помещать транспилируемый код
     output: {
         path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js'
+        filename: 'client.js',
+        publicPath: "/static/",
     },
     //настройка зависимостей(loaders)
     module: {
@@ -31,5 +41,11 @@ module.exports = {
             use: ['ts-loader']
         }]
     },
-    devtool: setupDevtool()
+    devtool: setupDevtool(),
+    plugins: IS_DEV
+    ? [
+        new CleanWebpackPlugin(),
+        new HotModuleReplacementPlugin(),
+    ]
+    : [],
 };
